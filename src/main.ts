178050f44e395c6c347f230da45d4c4e64544e09
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-10-09 18:03:16 -0700

    Step 26

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from '@/App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
 app.mount('#app')
 
 
